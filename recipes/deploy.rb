6521615272f95c7f2c4b53437365ebb603bab9af
Jackiefong <jackiefong@gmail.com>
2015-11-10 02:10:13 -0500

    Update deploy.rb

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -2,6 +2,9 @@ include_recipe 'deploy'
 deploy 'private_repo' do
   repo 'https://github.com/jackiefong/cookbook.git'
   deploy_to '/tmp/private_code'
-
+  symlink_before_migrate.clear
+  create_dirs_before_symlink.clear
+  purge_before_symlink.clear
+  symlinks.clear
   action :deploy
 end
